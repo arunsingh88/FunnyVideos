f931bfd4dcfc676b12073343c282cc7f58194852
Arun <arunsingh.official@gmail.com>
2017-07-13 08:39:34 +0530

    Youtube Player added to app

--- app/src/main/java/com/thinktanki/funnyvideos/MainActivity.java
+++ app/src/main/java/com/thinktanki/funnyvideos/MainActivity.java
@@ -1,13 +1,77 @@
 package com.thinktanki.funnyvideos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStyle;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+    private static final String YOUTUBE_VIDEO_CODE = "0Q-fCuUxSFU";
+    // YouTube player view
+    private YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+
+        // Initializing video player with developer key
+        youTubeView.initialize(MainActivity.this.getResources().getString(R.string.YOUTUBE_API_KEY), this);
+
     }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    getString(R.string.error_player), errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+
+            // loadVideo() will auto play video
+            // Use cueVideo() method, if you don't want to play it automatically
+            player.loadVideo(YOUTUBE_VIDEO_CODE);
+
+            // Hiding player controls
+            //player.setPlayerStyle(PlayerStyle.CHROMELESS);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(MainActivity.this.getResources().getString(R.string.YOUTUBE_API_KEY), this);
+        }
+    }
+
+    private YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+
 }
